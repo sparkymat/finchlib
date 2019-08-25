182c26a8a171714cf7548eb0d06fc314eac6bba3
Ajith Hussain <csy0013@googlemail.com>
2019-08-26 01:39:42 +1000

    Added CheckRequired

--- config/util.go
+++ config/util.go
@@ -1,15 +1,58 @@
 package config
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"os"
 	"reflect"
 	"strconv"
 )
 
+// CheckRequiredFields checks all fielsd of a struct recursiely, and returns an error if any fields
+// tagged with required: true, have a zero value
+func CheckRequiredFields(originalStruct interface{}) error {
+	originalValue := reflect.ValueOf(originalStruct)
+
+	if originalValue.Type().Kind() != reflect.Ptr && originalValue.Type().Kind() != reflect.Struct {
+		return errors.New("param is not a struct or pointer to struct")
+	}
+
+	var structValue reflect.Value
+	if originalValue.Type().Kind() == reflect.Ptr {
+		structValue = reflect.Indirect(originalValue)
+		if structValue.Kind() != reflect.Struct {
+			return errors.New("param is not a struct or pointer to struct")
+		}
+	} else {
+		structValue = originalValue
+	}
+	structType := structValue.Type()
+
+	for fieldNum := 0; fieldNum < structType.NumField(); fieldNum++ {
+		fieldValue := structValue.Field(fieldNum)
+		fieldType := structType.Field(fieldNum)
+
+		if fieldType.Type.Kind() == reflect.Struct || (fieldType.Type.Kind() == reflect.Ptr && reflect.Indirect(fieldValue).Type().Kind() == reflect.Struct) {
+			err := CheckRequiredFields(fieldValue)
+			if err != nil {
+				return err
+			}
+		} else {
+			if isRequired := fieldType.Tag.Get("required"); isRequired == "true" {
+				if fieldValue == reflect.Zero(fieldType.Type) {
+					return fmt.Errorf("Required field %v has a zero-value", fieldType.Name)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
 // UpdateStructFromEnv loops through all fields in a struct and replaces the value of any field with a `env:..` tag, with the specified environment variable
-// `originalValue` should be reflect.ValueOf a struct pointer
-func UpdateStructFromEnv(originalValue reflect.Value) {
+func UpdateStructFromEnv(originalStruct interface{}) {
+	originalValue := reflect.ValueOf(originalStruct)
 	structValue := reflect.Indirect(originalValue)
 	structType := structValue.Type()
 
